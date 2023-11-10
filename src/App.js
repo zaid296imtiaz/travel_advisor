d5222d59eb230b7d9c97bb398557b2a65e750a12
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2023-11-09 21:23:57 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { CssBaseline, Grid } from "@mui/material";
+
+import Header from "./Components/Header/Header";
+import Map from "./Components/Map/Map";
+import PlaceDetails from "./Components/PlaceDetails/PlaceDetails";
+
+const App = () => { 
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
