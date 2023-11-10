b9b182b8a5b6f423f3c9d57fd67b1bf3156d654a
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2023-11-09 21:30:40 -0500

    Initial layout

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import { CssBaseline, Grid } from "@mui/material";
 
 import Header from "./Components/Header/Header";
 import Map from "./Components/Map/Map";
+import List from "./Components/List/List";
 import PlaceDetails from "./Components/PlaceDetails/PlaceDetails";
 
-const App = () => { 
-    return(
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default App;
